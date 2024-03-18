344a9b9a9270df8fb017d5bc790249ab8205e19c
christophermccall <camccall24@gmail.com>
2024-03-18 13:47:15 -0400

    finished. it works!

--- new_file_reader.py
+++ new_file_reader.py
@@ -1,12 +1,27 @@
 import logging
+import os
 
 
-def right_name_reader():
+# def right_name_writer():
+#     try:
+#         with open("/Users/chris/pyprojects/PythonFundamentals.Exercises.Part8/new_file.txt", "a") as file:
+#             file.write("hi!!!")
+#             #for line in file:
+#                 #print(f"{line}", end="")
+#                  #file.writelines(["hey there"])
+#     except FileNotFoundError as e:
+#         print("that didn't work,man")
+#         logging.error(e)
+
+
+
+def name_writer(path,message):
     try:
-        file = open("new_file.txt")
-        for line in file:
-            print(f"{line}", end="")
-        file.close()
+        with open(path, "a") as file:
+            file.write("\n"+message)
+            #for line in file:
+                #print(f"{line}", end="")
+                 #file.writelines(["hey there"])
     except FileNotFoundError as e:
         print("that didn't work,man")
         logging.error(e)
@@ -23,5 +38,28 @@ def wrong_name_reader():
         logging.error(e)
 
 
-# right_name_reader()
+#right_name_reader()
 # wrong_name_reader()
+
+
+tree = os.listdir("/Users/chris/pyprojects/PythonFundamentals.Exercises.Part8/")
+
+def path_lister():
+    #holder for strings
+    file_list = ""
+    for item in tree:
+        if os.path.isfile(item):
+            file_list += f'./{item}\n'
+           # print()
+        else:
+            for file in os.listdir(item):
+                file_list += f'./{item}/{file}\n'
+               # print(f'./{item}/{file}')
+    return file_list
+
+
+name_writer("/Users/chris/pyprojects/PythonFundamentals.Exercises.Part8/new_file.txt", path_lister())
+
+#file_path_list =[os.listdir(child_dir) for child_dir in tree if os.path.isdir(child_dir)]
+
+#print(file_path_list)
--- tree.py
+++ tree.py
@@ -2,6 +2,23 @@ import logging
 import os
 
 #from file_reader import
+
+print(os.path.abspath('file_reader.py'))
+def right_name_reader():
+    try:
+        file = open("new_file.txt")
+        for line in file:
+            print(f"{line}", end="")
+        file.close()
+    except FileNotFoundError as e:
+        print("that didn't work,man")
+        logging.error(e)
+
+
+
+
+
+'''
 try:
     with open("/Users", 'r') as file:
         print(file)
@@ -9,3 +26,4 @@ try:
 
 except FileNotFoundError:
     print('Couldn\'t find the file!')
+'''
